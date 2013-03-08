d8b49139572ace8b19e54aeb3dc5cfeacdd12db1
Glyn Normington <gnormington@vmware.com>
2013-03-08 11:04:55 +0000

    copy resources earlier

--- lib/language_pack/virgo.rb
+++ lib/language_pack/virgo.rb
@@ -1,42 +1,15 @@
 module LanguagePack
   class Virgo < Java
-    def default_process_types
-      {
-          "web" => "./bin/startup.sh -clean"
-      }
-    end
-
-    def java_opts
-      # TODO proxy settings?
-      # Don't override Virgo's temp dir setting
-      opts = super.merge({"-Dhttp.port=" => "$VCAP_APP_PORT"})
-      opts.delete("-Djava.io.tmpdir=")
-      opts
-    end
-
-    def copy_resources
-      # Configure server.xml with variable HTTP port
-      run_with_err_output("cp -r #{File.expand_path('../../../resources/virgo', __FILE__)}/* #{build_path}")
-    end
-
-    def move_virgo_to_root
-      run_with_err_output("mv #{virgo_dir}/* . && rm -rf #{virgo_dir}")
-    end
-
-    def virgo_dir
-      ".virgo"
-    end
-
-    def remove_virgo_files
-      %w(notice.html epl-v10.html docs work [Aa]bout* pickup/org.eclipse.virgo.apps.*).each do |file|
-        Dir.glob("#{virgo_dir}/#{file}") do |entry|
-          FileUtils.rm_rf(entry)
-        end
+    def compile
+      Dir.chdir(build_path) do
+        install_java
+        install_virgo
+        remove_virgo_files
+        copy_resources
+        copy_webapp_to_virgo
+        move_virgo_to_root
+        setup_profiled
       end
-    end
-
-    def download_virgo(virgo_zip)
-      run_with_err_output("curl --silent --location #{VIRGO_URL} --output #{virgo_zip}")
     end
 
     def install_virgo
@@ -53,16 +26,43 @@ module LanguagePack
       end
     end
 
-    def compile
-      Dir.chdir(build_path) do
-        install_java
-        install_virgo
-        remove_virgo_files
-        copy_webapp_to_virgo
-        move_virgo_to_root
-        copy_resources
-        setup_profiled
+    def remove_virgo_files
+      %w(notice.html epl-v10.html docs work [Aa]bout* pickup/org.eclipse.virgo.apps.*).each do |file|
+        Dir.glob("#{virgo_dir}/#{file}") do |entry|
+          FileUtils.rm_rf(entry)
+        end
       end
+    end
+
+    def copy_resources
+      # Configure server.xml with variable HTTP port
+      run_with_err_output("cp -r #{File.expand_path('../../../resources/virgo', __FILE__)}/* #{virgo_dir}")
+    end
+
+    def move_virgo_to_root
+      run_with_err_output("mv #{virgo_dir}/* . && rm -rf #{virgo_dir}")
+    end
+
+    def default_process_types
+      {
+          "web" => "./bin/startup.sh -clean"
+      }
+    end
+
+    def java_opts
+      # TODO proxy settings?
+      # Don't override Virgo's temp dir setting
+      opts = super.merge({"-Dhttp.port=" => "$VCAP_APP_PORT"})
+      opts.delete("-Djava.io.tmpdir=")
+      opts
+    end
+
+     def virgo_dir
+      ".virgo"
+    end
+
+     def download_virgo(virgo_zip)
+      run_with_err_output("curl --silent --location #{VIRGO_URL} --output #{virgo_zip}")
     end
   end
 end
